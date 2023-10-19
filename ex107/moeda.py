a27091fba499819af65c289ae31ad5d7dc12b017
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2023-10-19 16:53:59 -0300

    Formatando moedas para Real

--- ex107/moeda.py
+++ ex107/moeda.py
@@ -1,11 +1,17 @@
+def moeda(n):
+    return f'R${n:.2f}'.replace('.', ',')
+
+
 def metade(n):
     n = n / 2
-    return n
+    return moeda(n)
+
 
 def dobro(n):
     n = n * 2
-    return n
+    return moeda(n)
+
 
 def aumentar(n, taxa):
     n = (n * taxa/100) + n
-    return n
+    return moeda(n)
--- ex107/teste.py
+++ ex107/teste.py
@@ -1,6 +1,6 @@
 import moeda
 
-preço = float(input('Digite o preço: '))
-print(f'A metade de R${preço} é R${moeda.metade(preço)}')
-print(f'O dobro de R${preço} é R${moeda.dobro(preço)}')
-print(f'Aumentando 10%, temos R${moeda.aumentar(preço, 10)}')
+preço = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda.moeda(preço)} é {moeda.metade(preço)}')
+print(f'O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(preço, 10)}')
